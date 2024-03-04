4a2d20be8e8f2941837f343af178beab12f85408
code2k13 <ashpat.api@gmail.com>
2024-03-04 04:12:27 +0000

    added support for stable diffusion Turbo

--- plugins/txt2img/sdturbo_txt2img.py
+++ plugins/txt2img/sdturbo_txt2img.py
@@ -3,9 +3,11 @@ from utils import create_temp_file, is_valid_filename
 from diffusers import AutoPipelineForText2Image
 import os
 
+
 class StableDiffusionTxt2Img(BaseTxt2Img):
 
     USE_CUDA = os.getenv('USE_CUDA')
+    USE_SD_TURBO_XL = os.getenv('USE_SD_TURBO_XL')
 
     def __generate_image(self, model, text: str):
         random_filename = create_temp_file(".jpg")
@@ -15,8 +17,11 @@ class StableDiffusionTxt2Img(BaseTxt2Img):
         return random_filename
 
     def generate_images(self, script_file: str):
-        image_model = AutoPipelineForText2Image.from_pretrained(
-            "stabilityai/sd-turbo")
+        if self.USE_SD_TURBO_XL == "true":
+            model_name = "stabilityai/sdxl-turbo"
+        else:
+            model_name = "stabilityai/sd-turbo"
+        image_model = AutoPipelineForText2Image.from_pretrained(model_name)
         if self.USE_CUDA == "true":
             image_model.to("cuda")
         image_video_dict = {}
@@ -25,8 +30,9 @@ class StableDiffusionTxt2Img(BaseTxt2Img):
                 if line.startswith("Image:"):
                     image_text = line.split("Image:")[1].strip()
                     if is_valid_filename(image_text):
-                         image_video_dict[image_text] = image_text
+                        image_video_dict[image_text] = image_text
                     else:
-                        image_file = self.__generate_image(image_model, image_text)
+                        image_file = self.__generate_image(
+                            image_model, image_text)
                         image_video_dict[image_text] = image_file
         return image_video_dict
